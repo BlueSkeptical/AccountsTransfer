3e77cfb63fb40bf1c9ed8ab9a2a15b31388e0b99
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2019-03-17 15:53:20 +0100

    Refactoring

--- src/main/java/com/bnk/accounts/ws/HttpClientTransferService.java
+++ src/main/java/com/bnk/accounts/ws/HttpClientTransferService.java
@@ -7,6 +7,7 @@ import com.bnk.accounts.TransferService;
 import com.bnk.accounts.Value;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
@@ -19,20 +20,21 @@ public class HttpClientTransferService implements TransferService {
 
     public static final int READ_TIMEOUT_MS = 5_000;
     public static final int CONNECT_TIMEOUT_MS = 5_000;
-    
+
     public static final String FROM_ACCOUNT_PARAMETER_NAME = "from";
     public static final String TO_ACCOUNT_PARAMETER_NAME = "to";
     public static final String AMOUNT_PARAMETER_NAME = "amount";
     public static final String CONTENT_TYPE = "text/plain";
     public static final String TRANSFER_RESOURCE_NAME = "transfer";
+    public static final int OK_HTTP_CODE = 200;
     public static final int BUSINESS_LOGIC_CONFLICT_HTTP_CODE = 409;
-    
+
     private final InetSocketAddress address;
     private final String basePath;
-    
+
     HttpClientTransferService(InetSocketAddress address, String basePath) {
-       this.address = address;
-       this.basePath = basePath;
+        this.address = address;
+        this.basePath = basePath;
     }
 
     /**
@@ -41,47 +43,45 @@ public class HttpClientTransferService implements TransferService {
     @Override
     public Value transfer(AccountNumber from, AccountNumber to, Value amount) {
         try {
-            final URL url = new URL( "http://" 
-                                    + address.getHostString() + ":" + address.getPort()
-                                    + basePath 
-                                    + "/" + TRANSFER_RESOURCE_NAME
-                                    + "?" + FROM_ACCOUNT_PARAMETER_NAME + "=" + from
-                                    + "&" + TO_ACCOUNT_PARAMETER_NAME + "=" + to
-                                    + "&" + AMOUNT_PARAMETER_NAME + "=" + amount);
+            final URL url = new URL("http://"
+                    + address.getHostString() + ":" + address.getPort()
+                    + basePath
+                    + "/" + TRANSFER_RESOURCE_NAME
+                    + "?" + FROM_ACCOUNT_PARAMETER_NAME + "=" + from
+                    + "&" + TO_ACCOUNT_PARAMETER_NAME + "=" + to
+                    + "&" + AMOUNT_PARAMETER_NAME + "=" + amount);
             final HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                
+
             con.setRequestMethod("POST");
             con.setRequestProperty("Content-Type", CONTENT_TYPE);
             con.setConnectTimeout(CONNECT_TIMEOUT_MS);
             con.setReadTimeout(READ_TIMEOUT_MS);
-            
-            if (con.getResponseCode() != 200) {
-                final BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-                String inputLine;
-                final StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                      content.append(inputLine);
+
+            if (con.getResponseCode() != OK_HTTP_CODE) {
+                final String responseErrorData = read(con.getErrorStream());
+                if (con.getResponseCode() == BUSINESS_LOGIC_CONFLICT_HTTP_CODE) {
+
+                    throw new TransferException(responseErrorData);
                 }
-                if(con.getResponseCode() == BUSINESS_LOGIC_CONFLICT_HTTP_CODE) {
-         
-                    throw new TransferException(content.toString());
-                }
-                throw new RuntimeException("Server error: " + con.getResponseCode() + content);
+                throw new RuntimeException("Server error: " + con.getResponseCode() + responseErrorData);
             }
-            final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            final StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                  content.append(inputLine);
-            }
-            return new Value(Long.parseLong(content.toString())); //TODO
-            
+            final String responseData = read(con.getInputStream());
+            return new Value(Long.parseLong(responseData));
+
         } catch (IOException ex) {
-             throw new RuntimeException(ex);
+            throw new RuntimeException(ex);
         }
     }
 
-
+    private String read(InputStream is) throws IOException {
+        final BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        String inputLine;
+        final StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        return content.toString();
+    }
 
     @Override
     public Account withdraw(Account from, Value amount) {
@@ -92,5 +92,5 @@ public class HttpClientTransferService implements TransferService {
     public Account deposit(Account to, Value amount) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
