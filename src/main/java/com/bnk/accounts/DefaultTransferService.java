49542bce769a106bd82e66c2a0df1421529276a5
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2019-03-17 15:59:12 +0100

    Refactoring

--- src/main/java/com/bnk/accounts/DefaultTransferService.java
+++ src/main/java/com/bnk/accounts/DefaultTransferService.java
@@ -17,7 +17,7 @@ public class DefaultTransferService implements TransferService {
     public Value transfer(AccountNumber accountFrom, AccountNumber to, Value amount) {
         
         
-        final Transaction<Order> transaction = logWithdrawtOrder(Transaction.newInstace(), repository.account(accountFrom), amount);
+        final Transaction<Order> transaction = logWithdrawOrder(Transaction.newInstace(), repository.account(accountFrom), amount);
         final Transaction<Order> resultTransaction = logDepositOrder(transaction, repository.account(to), amount);
         repository.commit(resultTransaction);
 
@@ -28,7 +28,7 @@ public class DefaultTransferService implements TransferService {
     @Override
     public Account withdraw(Account from, Value amount) {
         try {
-            final Transaction<Order> transaction = logWithdrawtOrder(Transaction.newInstace(), from, amount);
+            final Transaction<Order> transaction = logWithdrawOrder(Transaction.newInstace(), from, amount);
             repository.commit(transaction);
         } catch (Exception ex) {
             ////
@@ -52,7 +52,7 @@ public class DefaultTransferService implements TransferService {
             return transaction.log(depositOrder);
     }
     
-    private Transaction<Order> logWithdrawtOrder(Transaction<Order> transaction, Account from, Value amount) {
+    private Transaction<Order> logWithdrawOrder(Transaction<Order> transaction, Account from, Value amount) {
         if (from.balance().compareTo(amount) >= 0) {
             final Order order = new DefaultOrder(from.number(), amount.negate());
             return transaction.log(order);
