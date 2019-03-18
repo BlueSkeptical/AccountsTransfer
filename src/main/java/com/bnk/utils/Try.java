ea44330eb3c686a9d8aa8c8169fa94fa71b352f3
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2019-03-18 20:25:38 +0100

    Functional type Try basic implementation

---
+++ src/main/java/com/bnk/utils/Try.java
@@ -0,0 +1,57 @@
+package com.bnk.utils;
+
+import java.util.function.Function;
+
+
+public abstract class Try<T> {
+    
+    public static <T> Try<T> success(T value) {
+       return new Success<>(value); 
+    }
+    
+    public static <T> Try<T> failure(Exception exception) {
+       return new Failure<>(exception); 
+    }
+   
+    public abstract <S> Try<S> map(Function<T,S> fun);
+    
+    public abstract <S> Try<S> flatMap(Function<T,Try<S>> fun);
+    
+    private static class Success<T> extends Try<T> {
+
+        private final T value;
+        
+        public Success(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public <S> Try<S> map(Function<T, S> fun) {
+            return success(fun.apply(value));
+        }
+
+        @Override
+        public <S> Try<S> flatMap(Function<T, Try<S>> fun) {
+            return fun.apply(value);
+        }  
+    }
+    
+    private static class Failure<T> extends Try<T> {
+
+        private final Exception exception;
+        
+        public Failure(Exception exception) {
+            this.exception = exception;
+        }
+
+        @Override
+        public <S> Try<S> map(Function<T, S> fun) {
+            return failure(exception);
+        }
+
+        @Override
+        public <S> Try<S> flatMap(Function<T, Try<S>> fun) {
+            return failure(exception);
+        }
+    }
+}
