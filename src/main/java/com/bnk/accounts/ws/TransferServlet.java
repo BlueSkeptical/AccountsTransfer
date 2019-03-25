2e0e162f447e7f7261204a9f135ef374940638b0
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2019-03-26 00:16:40 +0100

    Servlet's code reworked towards FP

--- src/main/java/com/bnk/accounts/ws/TransferServlet.java
+++ src/main/java/com/bnk/accounts/ws/TransferServlet.java
@@ -27,22 +27,20 @@ public class TransferServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType(HttpClientTransferService.CONTENT_TYPE);
-        //TODO
-        final int fromAccountId = Integer.parseInt(request.getParameter(HttpClientTransferService.FROM_ACCOUNT_PARAMETER_NAME));
-        final int toAccountId = Integer.parseInt(request.getParameter(HttpClientTransferService.TO_ACCOUNT_PARAMETER_NAME));
-        final Value amount = new Value(Long.parseLong(request.getParameter(HttpClientTransferService.AMOUNT_PARAMETER_NAME)));
-        
-        final Try<Integer> result = transferService.transfer(new AccountNumber(fromAccountId), new AccountNumber(toAccountId), amount).run();
-        
-        result.io(p -> IO.effect(() -> { response.setStatus(HttpServletResponse.SC_OK);
+
+        Try.of(() -> Integer.parseInt(request.getParameter(HttpClientTransferService.FROM_ACCOUNT_PARAMETER_NAME)))
+           .flatMap(fromAccountId -> 
+                   Try.of(() -> Integer.parseInt(request.getParameter(HttpClientTransferService.TO_ACCOUNT_PARAMETER_NAME)))
+           .flatMap(toAccountId -> 
+                   Try.of(() -> new Value(Long.parseLong(request.getParameter(HttpClientTransferService.AMOUNT_PARAMETER_NAME))))
+           .flatMap(amount -> 
+                   transferService.transfer(new AccountNumber(fromAccountId), new AccountNumber(toAccountId), amount).run())))
+           .io(p -> IO.effect(() -> { response.setStatus(HttpServletResponse.SC_OK);
                                          write(response, p.toString()); }),
-                  ex -> IO.effect(() -> { response.setStatus(HttpServletResponse.SC_CONFLICT);
+               ex -> IO.effect(() -> { response.setStatus(HttpServletResponse.SC_CONFLICT);
                                         write(response, "ERR:" + ex.getMessage()); 
                                         Logger.getLogger(TransferServlet.class.getName()).log(Level.WARNING, ex.getMessage()); } )).run();
-        
     }
-
-  
     
     private static void write(HttpServletResponse response, String str) {
         try {
