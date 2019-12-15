6c7416842f83b1862ca8802ba7ef75036963a286
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2019-12-15 10:48:41 +0100

    Accounts repository test added

--- src/test/java/sample/accounts/DefaultAccountTest.java
+++ src/test/java/sample/accounts/DefaultAccountTest.java
@@ -2,14 +2,12 @@ package sample.accounts;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 public class DefaultAccountTest {
    
     @Test
-    public void should_have_zero_balance_with_empty_transactions_log() {
+    public void should_have_zero_balance_by_default() {
         final Account account = new DefaultAccount(new AccountNumber(1), new OwnerName("Name", "Surname"));
         assertEquals(Value.ZERO, account.balance());
     }
--- src/test/java/sample/accounts/SimpleAccountsRepositoryTest.java
+++ src/test/java/sample/accounts/SimpleAccountsRepositoryTest.java
@@ -1,5 +1,40 @@
 package sample.accounts;
 
-public class SimpleAccountsRepositoryTest {
+import java.util.Arrays;
 
+import org.junit.Test;
+
+import lajkonik.fp.IO;
+import lajkonik.fp.Try;
+
+import static lajkonik.fp.test.Assertions.require;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class SimpleAccountsRepositoryTest {
+    
+    private final static Account ACCOUNT_0 = new DefaultAccount(new AccountNumber(222), new OwnerName("Name", "Surname"));
+    
+    @Test
+    public void should_query_correct_account_for_known_account_number() {
+        final SimpleAccountsRepository ar = createTestRepository();
+        require(ar.account(new AccountNumber(222)).run(), v -> { assertEquals(ACCOUNT_0.number(), v.number());
+                                                                 assertEquals(new Value(500), v.balance()); });
+    }
+
+ 
+    @Test
+    public void should_fail_for_unknown_account_number() {
+        final SimpleAccountsRepository ar = createTestRepository();
+        final Try<? extends Account> result = ar.account(new AccountNumber(333)).run();
+        result.onResult(r -> IO.effect(() -> { fail(); }),
+                        ex -> IO.effect(() -> { assertTrue(ex instanceof TransferException); } ));
+    }
+
+    
+    private static SimpleAccountsRepository createTestRepository() {
+        return new SimpleAccountsRepository(Arrays.asList(ACCOUNT_0),
+                                            Arrays.asList(new DefaultOrder(ACCOUNT_0.number(), new Value(500))));
+    }
 }
