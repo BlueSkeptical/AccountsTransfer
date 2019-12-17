d2171072bebbe54e0f95498a1e1d9789c6435b70
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2019-12-17 18:07:40 +0100

    Fixing a concurrency issue

--- src/main/java/sample/utils/ThreadSafeExecution.java
+++ src/main/java/sample/utils/ThreadSafeExecution.java
@@ -5,13 +5,8 @@ import lajkonik.fp.*;
 
 public class ThreadSafeExecution {
 
-    public <T> Try<T> execute(IO<T> io) {
-        Try<T> result=null;
-        synchronized(this) {
-            result = io.run();
-        }
-        assert result != null;
-        return result;
+    public synchronized <T> Try<T>  execute(IO<T> io) {
+        return io.run();
     }
 
 }
